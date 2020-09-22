a75469bcda6b0a4f669566713f88161cc3fbb5fd
Jasper Alakoskela <jasper.alakosekla@gmail.com>
2020-09-22 09:46:09 +0300

    laitoin kuvia

--- kotisivut.js
+++ kotisivut.js
@@ -1,4 +1,14 @@
-const someLista = document.getElementById("somelista");
-const somet = document.getElementById("somet");
+function bigImg(x) {
+    x.style.height = "250px";
+    x.style.width = "250px";
+}
 
-function someFunktio() {}
+function smallImg(x) {
+    x.style.height = "35px";
+    x.style.width = "35px";
+}
+
+function mediumImg(x) {
+    x.style.height = "120px";
+    x.style.width = "120px";
+}
